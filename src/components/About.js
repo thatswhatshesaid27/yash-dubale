b2bd6b70c153b727e2eb5c4be2413b8757574e9c
Yash-D <90383046+thatswhatshesaid27@users.noreply.github.com>
2025-07-28 21:23:04 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -31,7 +31,7 @@ const About = () => {
             <div>
               <div className='text-[40px] font-teritiary text-gradient mb-2'>
                 { inView ?
-                  <CountUp start={0} end={2} duration={5}/>: null}
+                  <CountUp start={0} end={1} duration={10}/>: null}
               </div>
               <div className='font-primary text-sm tracking-[2px]'>
                 Years of<br/>
